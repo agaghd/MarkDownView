3fd8f0462c09e66c47896a644580c2a320accda2
agaghd <15150829710@163.com>
2018-03-10 19:30:34 +0800

    添加正则表达式。

--- app/src/main/java/io/github/agaghd/markdownview/utils/MarkDown.java
+++ app/src/main/java/io/github/agaghd/markdownview/utils/MarkDown.java
@@ -3,6 +3,8 @@ package io.github.agaghd.markdownview.utils;
 import android.text.SpannableStringBuilder;
 import android.widget.TextView;
 
+import java.util.regex.Pattern;
+
 /**
  * author : wjy
  * time   : 2018/03/09
@@ -19,7 +21,46 @@ public class MarkDown {
         return spannableStringBuilder;
     }
 
-    private MarkDown(){
+    public static class Patterns {
+        /**
+         * MarkDown 1~3级标题正则
+         */
+        public static final Pattern H1 = Pattern.compile("^#{1}[^#]+.*$");
+        public static final Pattern H2 = Pattern.compile("^#{2}[^#]+.*$");
+        public static final Pattern H3 = Pattern.compile("^#{3}[^#]+.*$");
+
+        /**
+         * MarkDown 图片正则
+         */
+        public static final Pattern IMAGE = Pattern.compile("^!\\[.*\\]\\(.*\\)$");
+
+        /**
+         * MarkDown 超链接正则
+         */
+        public static final Pattern HYPER_LINK = Pattern.compile("^\\[.*\\]\\(.*\\)$");
+
+        /**
+         * MarkDown 斜体正则
+         */
+        public static final Pattern ITALIC = Pattern.compile("\\*[^\\*]+\\*$");
+
+        /**
+         * MarkDown 粗体正则
+         */
+        public static final Pattern BOLD = Pattern.compile("\\*{2}[^\\*]+\\*{2}");
+
+        /**
+         * MarkDown 有序列表正则
+         */
+        public static final Pattern OL = Pattern.compile("^\\d\\..*$");
+
+        /**
+         * MarkDown 无序列表正则
+         */
+        public static final Pattern UL = Pattern.compile("^-.*$");
+    }
+
+    private MarkDown() {
 
     }
 }
