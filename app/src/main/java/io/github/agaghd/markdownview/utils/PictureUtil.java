1ad0b082576c4c1e501e76991c3cf193ad4f3654
agaghd <15150829710@163.com>
2018-03-10 19:31:27 +0800

    添加图片处理工具，添加通过最大宽度限制压缩图片尺寸的方法。

---
+++ app/src/main/java/io/github/agaghd/markdownview/utils/PictureUtil.java
@@ -0,0 +1,40 @@
+package io.github.agaghd.markdownview.utils;
+
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.WorkerThread;
+
+/**
+ * author : wjy
+ * time   : 2018/03/09
+ * desc   : 图片处理工具类
+ */
+
+public class PictureUtil {
+
+    /**
+     * 压缩宽度大于最大宽度限定的图片
+     * 耗时方法，建议运行在子线程
+     *
+     * @param bitmap   原图片
+     * @param maxWidth 最大宽度限定
+     * @return 以给定的最大宽度限定按比例缩小的图片
+     */
+    @WorkerThread
+    public static Bitmap compressBitmapByWidth(
+            @NonNull Bitmap bitmap,
+            @IntRange(from = 1, to = Integer.MAX_VALUE) int maxWidth) {
+        int width = bitmap.getWidth();
+        if (width <= maxWidth) {
+            return bitmap;
+        } else {
+            int height = bitmap.getHeight() * maxWidth / width;
+            return ThumbnailUtils.extractThumbnail(bitmap, maxWidth, height);
+        }
+    }
+
+    private PictureUtil() {
+    }
+}
